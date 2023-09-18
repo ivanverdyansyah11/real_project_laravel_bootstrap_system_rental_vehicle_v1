6d4dcfc2d038f3fb632df2a6f5ae1052366c04b4
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-09-18 23:41:13 +0800

    Progress: Finish UI Login page

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Nusa Kendala Rental Kendaraan | Halaman Login</title>
+    <title>Nusa Kendala Rental Kendaraan | Halaman {{ $title }}</title>
 
     {{-- STYLE CSS --}}
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('template.main');
+Route::get('/login', function () {
+    return view('login.index');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
 });
