05cd0d9bbf5e06f31cc2d69f529138ff6283aa2e
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-10-04 12:49:31 +0800

    Progress: Old Value Pengembalian

--- resources/views/pengembalian/restoration.blade.php
+++ resources/views/pengembalian/restoration.blade.php
@@ -80,9 +80,15 @@
                                         <select id="jenis_pembayaran" class="input" name="jenis_pembayaran"
                                             value="{{ old('jenis_pembayaran') }}">
                                             <option value="-">Pilih jenis pembayaran</option>
-                                            <option value="lunas">Lunas</option>
-                                            <option value="dp">DP</option>
-                                            <option value="belum bayar">Belum Bayar</option>
+                                            <option value="lunas"
+                                                {{ old('jenis_pembayaran') == 'lunas' ? 'selected' : '' }}>Lunas
+                                            </option>
+                                            <option value="dp" {{ old('jenis_pembayaran') == 'dp' ? 'selected' : '' }}>
+                                                DP
+                                            </option>
+                                            <option value="belum bayar"
+                                                {{ old('jenis_pembayaran') == 'belum bayar' ? 'selected' : '' }}>Belum
+                                                Bayar</option>
                                         </select>
                                         @error('jenis_pembayaran')
                                             <p class="caption-error mt-2">{{ $message }}</p>
@@ -92,17 +98,39 @@
                                 <div class="col-md-6 mb-4">
                                     <div class="input-wrapper">
                                         <label for="metode_bayar">Metode Pembayaran</label>
-                                        <select id="metode_bayar" class="input" name="metode_bayar"
-                                            value="{{ old('metode_bayar') }}">
+                                        <select id="metode_bayar" class="input" name="metode_bayar">
                                             <option value="-">Pilih metode pembayaran</option>
-                                            <option value="transfer bank">Transfer Bank</option>
-                                            <option value="internet banking">Internet Banking (E-Banking)</option>
-                                            <option value="mobile banking">Mobile Banking</option>
-                                            <option value="virtual account">Virtual Account (VA)</option>
-                                            <option value="online credit card">Online Credit Card</option>
-                                            <option value="rekening bersama">Rekening Bersama (Rekber)</option>
-                                            <option value="payPal">PayPal</option>
-                                            <option value="e-money">e-Money</option>
+                                            <option value="transfer bank"
+                                                {{ old('metode_bayar') == 'transfer bank' ? 'selected' : '' }}>Transfer
+                                                Bank
+                                            </option>
+                                            <option value="internet banking"
+                                                {{ old('metode_bayar') == 'internet banking' ? 'selected' : '' }}>Internet
+                                                Banking
+                                                (E-Banking)</option>
+                                            <option value="mobile banking"
+                                                {{ old('metode_bayar') == 'mobile banking' ? 'selected' : '' }}>Mobile
+                                                Banking
+                                            </option>
+                                            <option value="virtual account"
+                                                {{ old('metode_bayar') == 'virtual account' ? 'selected' : '' }}>Virtual
+                                                Account
+                                                (VA)
+                                            </option>
+                                            <option value="online credit card"
+                                                {{ old('metode_bayar') == 'online credit card' ? 'selected' : '' }}>Online
+                                                Credit
+                                                Card
+                                            </option>
+                                            <option value="rekening bersama"
+                                                {{ old('metode_bayar') == 'rekening bersama' ? 'selected' : '' }}>Rekening
+                                                Bersama
+                                                (Rekber)</option>
+                                            <option value="payPal" {{ old('metode_bayar') == 'payPal' ? 'selected' : '' }}>
+                                                PayPal</option>
+                                            <option value="e-money"
+                                                {{ old('metode_bayar') == 'e-money' ? 'selected' : '' }}>
+                                                e-Money</option>
                                         </select>
                                         @error('metode_bayar')
                                             <p class="caption-error mt-2">{{ $message }}</p>
@@ -113,7 +141,7 @@
                                     <div class="input-wrapper">
                                         <label for="tanggal_kembali">Pengembalian Tanggal</label>
                                         <input type="date" id="tanggal_kembali" name="tanggal_kembali" class="input"
-                                            autocomplete="off">
+                                            autocomplete="off" value="{{ old('tanggal_kembali') }}">
                                         @error('tanggal_kembali')
                                             <p class="caption-error mt-2">{{ $message }}</p>
                                         @enderror
@@ -133,7 +161,7 @@
                                     <div class="input-wrapper">
                                         <label for="kilometer_kembali">Kilometer Kembali</label>
                                         <input type="number" id="kilometer_kembali" name="kilometer_kembali"
-                                            class="input" autocomplete="off">
+                                            class="input" autocomplete="off" value="{{ old('kilometer_kembali') }}">
                                         @error('kilometer_kembali')
                                             <p class="caption-error mt-2">{{ $message }}</p>
                                         @enderror
@@ -143,7 +171,7 @@
                                     <div class="input-wrapper">
                                         <label for="bensin_kembali">Bensin Kembali</label>
                                         <input type="number" id="bensin_kembali" name="bensin_kembali" class="input"
-                                            autocomplete="off">
+                                            autocomplete="off" value="{{ old('bensin_kembali') }}">
                                         @error('bensin_kembali')
                                             <p class="caption-error mt-2">{{ $message }}</p>
                                         @enderror
@@ -154,8 +182,11 @@
                                         <label for="ketepatan_waktu">Ketepatan Waktu</label>
                                         <select id="ketepatan_waktu" name="ketepatan_waktu" class="input">
                                             <option value="-">Pilih ketepatan pengembalian</option>
-                                            <option value="tepat">Tepat</option>
-                                            <option value="tidak tepat">Tidak Tepat</option>
+                                            <option value="tepat"
+                                                {{ old('ketepatan_waktu') == 'tepat' ? 'selected' : '' }}>Tepat</option>
+                                            <option value="tidak tepat"
+                                                {{ old('ketepatan_waktu') == 'tidak tepat' ? 'selected' : '' }}>Tidak Tepat
+                                            </option>
                                         </select>
                                         @error('ketepatan_waktu')
                                             <p class="caption-error mt-2">{{ $message }}</p>
@@ -166,7 +197,7 @@
                                     <div class="input-wrapper">
                                         <label for="terlambat">Terlambat (Max 3 Jam)</label>
                                         <input type="number" id="terlambat" name="terlambat" class="input"
-                                            autocomplete="off">
+                                            autocomplete="off" value="{{ old('terlambat') }}">
                                         @error('terlambat')
                                             <p class="caption-error mt-2">{{ $message }}</p>
                                         @enderror
@@ -175,12 +206,14 @@
                                 <div class="col-md-6 mb-4">
                                     <div class="input-wrapper">
                                         <label for="sarung_jok">Sarung Jok</label>
-                                        <select id="sarung_jok" class="input" name="sarung_jok"
-                                            value="{{ old('sarung_jok') }}">
+                                        <select id="sarung_jok" class="input" name="sarung_jok">
                                             <option value="-">Pilih kelengkapan sarung jok</option>
-                                            <option value="ada">Ada</option>
-                                            <option value="tidak ada">Tidak Ada</option>
-                                            <option value="kosong">Kosong</option>
+                                            <option value="ada" {{ old('sarung_jok') == 'ada' ? 'selected' : '' }}>Ada
+                                            </option>
+                                            <option value="tidak ada"
+                                                {{ old('sarung_jok') == 'tidak ada' ? 'selected' : '' }}>Tidak Ada</option>
+                                            <option value="kosong" {{ old('sarung_jok') == 'kosong' ? 'selected' : '' }}>
+                                                Kosong</option>
                                         </select>
                                         @error('sarung_jok')
                                             <p class="caption-error mt-2">{{ $message }}</p>
@@ -193,9 +226,12 @@
                                         <select id="karpet" class="input" name="karpet"
                                             value="{{ old('karpet') }}">
                                             <option value="-">Pilih kelengkapan karpet</option>
-                                            <option value="ada">Ada</option>
-                                            <option value="tidak ada">Tidak Ada</option>
-                                            <option value="kosong">Kosong</option>
+                                            <option value="ada" {{ old('karpet') == 'ada' ? 'selected' : '' }}>Ada
+                                            </option>
+                                            <option value="tidak ada"
+                                                {{ old('karpet') == 'tidak ada' ? 'selected' : '' }}>Tidak Ada</option>
+                                            <option value="kosong" {{ old('karpet') == 'kosong' ? 'selected' : '' }}>
+                                                Kosong</option>
                                         </select>
                                         @error('karpet')
                                             <p class="caption-error mt-2">{{ $message }}</p>
@@ -208,24 +244,32 @@
                                         <select id="kondisi_kendaraan" class="input" name="kondisi_kendaraan"
                                             value="{{ old('kondisi_kendaraan') }}">
                                             <option value="-">Pilih kondisi kendaraan</option>
-                                            <option value="baik">Baik</option>
-                                            <option value="rusak ringan">Rusak Ringan</option>
-                                            <option value="rusak berat">Rusak Berat</option>
+                                            <option value="baik"
+                                                {{ old('kondisi_kendaraan') == 'baik' ? 'selected' : '' }}>Baik</option>
+                                            <option value="rusak ringan"
+                                                {{ old('kondisi_kendaraan') == 'rusak ringan' ? 'selected' : '' }}>Rusak
+                                                Ringan</option>
+                                            <option value="rusak berat"
+                                                {{ old('kondisi_kendaraan') == 'rusak berat' ? 'selected' : '' }}>Rusak
+                                                Berat</option>
                                         </select>
                                         @error('kondisi_kendaraan')
                                             <p class="caption-error mt-2">{{ $message }}</p>
                                         @enderror
                                     </div>
                                 </div>
-                                <div class="col-md-6 mb-4">
+                                <div class="col-md-6">
                                     <div class="input-wrapper">
                                         <label for="ban_serep">Ban Serep</label>
                                         <select id="ban_serep" class="input" name="ban_serep"
                                             value="{{ old('ban_serep') }}">
                                             <option value="-">Pilih ban serep</option>
-                                            <option value="ada">Ada</option>
-                                            <option value="tidak ada">Tidak Ada</option>
-                                            <option value="kosong">Kosong</option>
+                                            <option value="ada" {{ old('ban_serep') == 'ada' ? 'selected' : '' }}>Ada
+                                            </option>
+                                            <option value="tidak ada"
+                                                {{ old('ban_serep') == 'tidak ada' ? 'selected' : '' }}>Tidak Ada</option>
+                                            <option value="kosong" {{ old('ban_serep') == 'kosong' ? 'selected' : '' }}>
+                                                Kosong</option>
                                         </select>
                                         @error('ban_serep')
                                             <p class="caption-error mt-2">{{ $message }}</p>
@@ -236,7 +280,7 @@
                                     <div class="input-wrapper">
                                         <label for="biaya_tambahan">Biaya Tambahan</label>
                                         <input type="number" id="biaya_tambahan" name="biaya_tambahan" class="input"
-                                            autocomplete="off">
+                                            autocomplete="off" value="{{ old('biaya_tambahan') }}">
                                         @error('biaya_tambahan')
                                             <p class="caption-error mt-2">{{ $message }}</p>
                                         @enderror
@@ -246,7 +290,7 @@
                                     <div class="input-wrapper">
                                         <label for="keterangan">Keterangan</label>
                                         <input type="text" id="keterangan" name="keterangan" class="input"
-                                            autocomplete="off">
+                                            autocomplete="off" value="{{ old('keterangan') }}">
                                         @error('keterangan')
                                             <p class="caption-error mt-2">{{ $message }}</p>
                                         @enderror
