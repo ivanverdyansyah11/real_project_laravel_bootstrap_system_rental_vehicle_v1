6909744402b1395407af1a8281a4f0b5feda331d
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-23 21:23:45 +0800

    Progress: Update Add Try Catch Logger Servis Controller

--- app/Http/Controllers/ServisController.php
+++ app/Http/Controllers/ServisController.php
@@ -47,41 +47,46 @@ class ServisController extends Controller
 
     public function checkAction($id, Request $request)
     {
-        if ($request->air_accu == '' || $request->air_waiper == '' || $request->ban == '' || $request->oli == '') {
-            return redirect(route('servis.check', $id))->with('failed', 'Isi Form Input Kelengkapan Kondisi Kendaraan Terlebih Dahulu!');
-        }
-
-        $validatedData = $request->validate([
-            'tanggal_servis' => 'required|date',
-            'kilometer_sebelum' => 'required|string',
-            'kilometer_setelah' => 'required|string',
-            'air_accu' => 'required|string',
-            'air_waiper' => 'required|string',
-            'ban' => 'required|string',
-            'oli' => 'required|string',
-            'total_bayar' => 'nullable|string',
-            'keterangan' => 'nullable|string',
-        ]);
-
-        $validatedData['kendaraans_id'] = $id;
-
-        $kendaraan = Kendaraan::where('id', $id)->first()->update([
-            'kilometer' => $validatedData['kilometer_setelah'],
-            'kilometer_saat_ini' => $validatedData['kilometer_setelah'],
-            'status' => 'ready',
-        ]);
-
-        $servis = Servis::create($validatedData);
-        $servisID = Servis::latest()->first();
-        $laporan = Laporan::create([
-            'penggunas_id' => auth()->user()->id,
-            'relations_id' => $servisID->id,
-            'kategori_laporan' => 'servis',
-        ]);
-
-        if ($servis && $kendaraan && $laporan) {
-            return redirect(route('servis'))->with('success', 'Berhasil Melakukan Servis Kendaraan!');
-        } else {
+        try {
+            if ($request->air_accu == '' || $request->air_waiper == '' || $request->ban == '' || $request->oli == '') {
+                return redirect(route('servis.check', $id))->with('failed', 'Isi Form Input Kelengkapan Kondisi Kendaraan Terlebih Dahulu!');
+            }
+    
+            $validatedData = $request->validate([
+                'tanggal_servis' => 'required|date',
+                'kilometer_sebelum' => 'required|string',
+                'kilometer_setelah' => 'required|string',
+                'air_accu' => 'required|string',
+                'air_waiper' => 'required|string',
+                'ban' => 'required|string',
+                'oli' => 'required|string',
+                'total_bayar' => 'nullable|string',
+                'keterangan' => 'nullable|string',
+            ]);
+    
+            $validatedData['kendaraans_id'] = $id;
+    
+            $kendaraan = Kendaraan::where('id', $id)->first()->update([
+                'kilometer' => $validatedData['kilometer_setelah'],
+                'kilometer_saat_ini' => $validatedData['kilometer_setelah'],
+                'status' => 'ready',
+            ]);
+    
+            $servis = Servis::create($validatedData);
+            $servisID = Servis::latest()->first();
+            $laporan = Laporan::create([
+                'penggunas_id' => auth()->user()->id,
+                'relations_id' => $servisID->id,
+                'kategori_laporan' => 'servis',
+            ]);
+    
+            if ($servis && $kendaraan && $laporan) {
+                return redirect(route('servis'))->with('success', 'Berhasil Melakukan Servis Kendaraan!');
+            } else {
+                return redirect(route('servis'))->with('failed', 'Gagal Melakukan Servis Kendaraan!');
+            }
+        } catch (\Exception $e) {
+            logger($e->getMessage());
             return redirect(route('servis'))->with('failed', 'Gagal Melakukan Servis Kendaraan!');
         }
     }
