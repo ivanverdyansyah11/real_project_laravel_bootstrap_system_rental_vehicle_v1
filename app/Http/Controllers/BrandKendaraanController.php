c92cbf3ad92a6373c1b94a08e11d3c32dbe00273
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-23 21:22:02 +0800

    Progress: Update Add Try Catch Logger Brand Controller

--- app/Http/Controllers/BrandKendaraanController.php
+++ app/Http/Controllers/BrandKendaraanController.php
@@ -33,41 +33,51 @@ class BrandKendaraanController extends Controller
 
     function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'nama' => 'required|string|max:255',
-        ]);
-
-        $brand = BrandKendaraan::create($validatedData);
-
-        if ($brand) {
-            return redirect(route('brandKendaraan'))->with('success', 'Berhasil Tambah Brand Kendaraan!');
-        } else {
+        try {
+            $validatedData = $request->validate([
+                'nama' => 'required|string|max:255',
+            ]);
+            $brand = BrandKendaraan::create($validatedData);
+            if ($brand) {
+                return redirect(route('brandKendaraan'))->with('success', 'Berhasil Tambah Brand Kendaraan!');
+            } else {
+                return redirect(route('brandKendaraan'))->with('failed', 'Gagal Tambah Brand Kendaraan!');
+            }
+        } catch (\Exception $e) {
+            logger($e->getMessage());
             return redirect(route('brandKendaraan'))->with('failed', 'Gagal Tambah Brand Kendaraan!');
         }
     }
 
     function update($id, Request $request)
     {
-        $validatedData = $request->validate([
-            'nama' => 'required|string|max:255',
-        ]);
-
-        $brand = BrandKendaraan::where('id', $id)->first()->update($validatedData);
-
-        if ($brand) {
-            return redirect(route('brandKendaraan'))->with('success', 'Berhasil Update Brand Kendaraan!');
-        } else {
+        try {
+            $validatedData = $request->validate([
+                'nama' => 'required|string|max:255',
+            ]);
+            $brand = BrandKendaraan::where('id', $id)->first()->update($validatedData);
+            if ($brand) {
+                return redirect(route('brandKendaraan'))->with('success', 'Berhasil Update Brand Kendaraan!');
+            } else {
+                return redirect(route('brandKendaraan'))->with('failed', 'Gagal Update Brand Kendaraan!');
+            }
+        } catch (\Exception $e) {
+            logger($e->getMessage());
             return redirect(route('brandKendaraan'))->with('failed', 'Gagal Update Brand Kendaraan!');
         }
     }
 
     function delete($id)
     {
-        $brand = BrandKendaraan::where('id', $id)->first()->delete();
-
-        if ($brand) {
-            return redirect(route('brandKendaraan'))->with('success', 'Berhasil Hapus Brand Kendaraan!');
-        } else {
+        try {
+            $brand = BrandKendaraan::where('id', $id)->first()->delete();
+            if ($brand) {
+                return redirect(route('brandKendaraan'))->with('success', 'Berhasil Hapus Brand Kendaraan!');
+            } else {
+                return redirect(route('brandKendaraan'))->with('failed', 'Gagal Hapus Brand Kendaraan!');
+            }
+        } catch (\Exception $e) {
+            logger($e->getMessage());
             return redirect(route('brandKendaraan'))->with('failed', 'Gagal Hapus Brand Kendaraan!');
         }
     }
