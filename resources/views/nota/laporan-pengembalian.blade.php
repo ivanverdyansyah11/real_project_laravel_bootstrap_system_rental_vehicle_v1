a060b69dbb6106b158e69dfd78101840ec48d393
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-02-24 11:09:55 +0800

    Progress: Revision Laporan Pengembalian Page Nota

--- resources/views/nota/laporan-pengembalian.blade.php
+++ resources/views/nota/laporan-pengembalian.blade.php
@@ -1,150 +1,3 @@
-{{-- @extends('template.main')
-
-@section('content')
-    <div class="report-nota row justify-content-center my-5">
-        <div class="col-md-9 col-lg-7 col-xl-6">
-            <div class="nota-paper mx-3 mx-md-0">
-                <div class="wrapper d-flex justify-content-between align-items-end">
-                    <img src="{{ asset('assets/img/brand/brand-text.svg') }}" alt="Brand Nusa Kendala Logo Teks"
-                        class="img-fluid login-brand d-none d-md-inline-block" draggable="false" width="240">
-                    <p class="paragraph">Petugas: {{ $laporan->pengguna->nama_lengkap }}</p>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <h5 class="title">Data Pengembalian Kendaraan</h5>
-                        <table class="table table-bordered">
-                            <tr>
-                                <td scope="col">Nama Pelanggan</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->pelepasan_pemesanan->pemesanan->pelanggan->nama }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Nomor Telepon</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->pelepasan_pemesanan->pemesanan->pelanggan->nomor_telepon }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Nomor Plat</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->pelepasan_pemesanan->kendaraan->nomor_plat }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Jenis Pembayaran</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->jenis_pembayaran }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Total Bayar</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    Rp. {{ $pengembalian->total_bayar }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Metode Bayar</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->metode_bayar }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Tanggal Kembali</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->tanggal_kembali }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Kilometer Kembali</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->kilometer_kembali }} km</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Bensin Kembali</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->bensin_kembali }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Ketepatan Waktu</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->ketepatan_waktu }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Terlambat</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->terlambat ?: '-' }} hari</td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="col-12">
-                        <h5 class="title">Data Kelengkapan Pengembalian Kendaraan</h5>
-                        <table class="table table-bordered">
-                            <tr>
-                                <td scope="col">Sarung Jok</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">{{ $pengembalian->sarung_jok }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Karpet</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">{{ $pengembalian->karpet }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Kondisi Kendaraan</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">{{ $pengembalian->kondisi_kendaraan }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Ban Serep</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">{{ $pengembalian->ban_serep }}</td>
-                            </tr>
-                        </table>
-                    </div>
-                    <div class="col-12">
-                        <h5 class="title">Data Kelengkapan Pelepasan Kendaraan</h5>
-                        <table class="table table-bordered">
-                            <tr>
-                                <td scope="col">Sarung Jok</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">{{ $pengembalian->pelepasan_pemesanan->sarung_jok }}
-                                </td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Karpet</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">{{ $pengembalian->pelepasan_pemesanan->karpet }}</td>
-                            </tr>
-                            <tr>
-                                <td scope="col">Kondisi Kendaraan</td>
-                                <td scope="col-1">:</td>
-                                <td scope="col" class="text-end">
-                                    {{ $pengembalian->pelepasan_pemesanan->kondisi_kendaraan }}</td>
-                            </tr>
-                            @if ($pengembalian->pelepasan_pemesanan->ban_serep)
-                                <tr>
-                                    <td scope="col">Ban Serep</td>
-                                    <td scope="col-1">:</td>
-                                    <td scope="col" class="text-end">{{ $pengembalian->pelepasan_pemesanan->ban_serep }}
-                                    </td>
-                                </tr>
-                            @endif
-                        </table>
-                    </div>
-                </div>
-                <p class="copyright">Diproduksi oleh Nusa Kendala Rental Kendaraan</p>
-            </div>
-        </div>
-    </div>
-@endsection --}}
-
-
 @extends('template.main')
 
 @section('content')
@@ -198,8 +51,7 @@
                                 <div class="input-wrapper">
                                     <label for="total_bayar">Total Bayar Sebelumnya</label>
                                     <input type="text" id="total_bayar" class="input" autocomplete="off"
-                                        value="{{ $pengembalian->pelepasan_pemesanan->pembayaran_pemesanan->total_bayar ?: '0' }}"
-                                        readonly>
+                                        value="Rp. {{ $pengembalian->pelepasan_pemesanan->pembayaran_pemesanan->total_bayar ? number_format($pengembalian->pelepasan_pemesanan->pembayaran_pemesanan->total_bayar, 2, ",", ".") : '0' }}" readonly>
                                 </div>
                             </div>
                             <div class="col-md-6 mb-4">
@@ -214,7 +66,7 @@
                                 <div class="input-wrapper">
                                     <label for="total_tarif_sewa">Total Tarif Sewa</label>
                                     <input type="text" id="total_tarif_sewa" class="input"
-                                        value="{{ $pengembalian->pelepasan_pemesanan->pembayaran_pemesanan->total_tarif_sewa }}"
+                                        value="Rp. {{ number_format($pengembalian->pelepasan_pemesanan->pembayaran_pemesanan->total_tarif_sewa, 2, ",", ".") }}"
                                         readonly>
                                 </div>
                             </div>
@@ -242,8 +94,8 @@
                             <div class="col-md-6 mb-4">
                                 <div class="input-wrapper">
                                     <label for="total_bayar">Total Bayar</label>
-                                    <input type="number" id="total_bayar" class="input"
-                                        value="{{ $pengembalian->total_bayar }}" readonly>
+                                    <input type="text" id="total_bayar" class="input"
+                                        value="Rp. {{ number_format($pengembalian->total_bayar, 2, ",", ".") }}" readonly>
                                 </div>
                             </div>
                             <div class="col-md-6 mb-4">
@@ -255,9 +107,9 @@
                             </div>
                             <div class="col-md-6 mb-4">
                                 <div class="input-wrapper">
-                                    <label for="bensin_kembali">Bensin Kembali (Liter)</label>
-                                    <input type="number" id="bensin_kembali" class="input"
-                                        value="{{ $pengembalian->bensin_kembali }}" readonly>
+                                    <label for="bensin_kembali">Bensin Kembali</label>
+                                    <input type="text" id="bensin_kembali" class="input"
+                                        value="{{ $pengembalian->bensin_kembali }} Strip Bar" readonly>
                                 </div>
                             </div>
                             <div class="col-md-6 mb-4">
@@ -305,9 +157,8 @@
                             <div class="col-md-6 mb-4">
                                 <div class="input-wrapper">
                                     <label for="biaya_tambahan">Biaya Tambahan</label>
-                                    <input type="number" id="biaya_tambahan" class="input"
-                                        value="{{ $pengembalian->biaya_tambahan ? $pengembalian->biaya_tambahan : '-' }}"
-                                        readonly>
+                                    <input type="text" id="biaya_tambahan" class="input"
+                                        value="Rp. {{ $pengembalian->biaya_tambahan ? number_format($pengembalian->biaya_tambahan, 2, ",", ".") : '0' }}" readonly>
                                 </div>
                             </div>
                             <div class="col-md-6 mb-4">
