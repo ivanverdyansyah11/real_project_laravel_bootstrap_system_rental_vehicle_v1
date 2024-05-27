f7637b51dbce5b04dd4deb17401347e1d7b7e479
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-05-27 13:54:22 +0800

    Progress: Update transaction pajak

--- resources/views/pajak/transaction.blade.php
+++ resources/views/pajak/transaction.blade.php
@@ -117,6 +117,9 @@
                                 <label for="metode_bayar">Metode Pembayaran</label>
                                 <select id="metode_bayar" class="input" name="metode_bayar" required>
                                     <option value="">Pilih metode pembayaran</option>
+                                    <option value="cash"
+                                        {{ old('metode_bayar') == 'cash' ? 'selected' : '' }}>Cash
+                                    </option>
                                     <option value="transfer bank"
                                         {{ old('metode_bayar') == 'transfer bank' ? 'selected' : '' }}>Transfer Bank
                                     </option>
