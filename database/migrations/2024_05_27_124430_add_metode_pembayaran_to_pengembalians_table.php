91b4a240c143591222966d761b0e37a2916ca6fe
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-05-27 22:30:09 +0800

    Progress: Update enum metode bayar pengembalian migration

--- database/migrations/2024_05_27_124430_add_metode_pembayaran_to_pengembalians_table.php
+++ database/migrations/2024_05_27_124430_add_metode_pembayaran_to_pengembalians_table.php
@@ -14,10 +14,10 @@ class AddMetodePembayaranToPengembaliansTable extends Migration
      */
     public function up()
     {
-        // Schema::table('pengembalians', function (Blueprint $table) {
-        //     $table->enum('metode_bayar', ['cash', 'transfer bank', 'internet banking', 'mobile banking', 'virtual account', 'online credit card', 'rekening bersama', 'payPal', 'e-money'])->nullable();
-        // });
-        DB::statement("ALTER TABLE `pengembalians` MODIFY  `metode_bayar` ENUM('cash', 'transfer bank', 'internet banking', 'mobile banking') NULL");
+//         Schema::table('pengembalians', function (Blueprint $table) {
+//             $table->enum('metode_bayar', ['cash', 'transfer bank', 'internet banking', 'mobile banking', 'virtual account', 'online credit card', 'rekening bersama', 'payPal', 'e-money'])->nullable();
+//         });
+        DB::statement("ALTER TABLE `pengembalians` MODIFY  `metode_bayar` ENUM('cash', 'transfer bank', 'internet banking', 'mobile banking', 'virtual account', 'online credit card', 'rekening bersama', 'payPal', 'e-money') NULL");
     }
 
     /**
