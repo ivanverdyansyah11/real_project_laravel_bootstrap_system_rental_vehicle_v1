d7ca753fd8c299ecb0e213e23934dfce9d897f2a
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-05-28 02:24:38 +0800

    Progress: update add with trashed pajak model

--- app/Models/Pajak.php
+++ app/Models/Pajak.php
@@ -12,6 +12,6 @@ class Pajak extends Model
 
     public function kendaraan()
     {
-        return $this->belongsTo(Kendaraan::class, 'kendaraans_id');
+        return $this->belongsTo(Kendaraan::class, 'kendaraans_id')->withTrashed();
     }
 }
