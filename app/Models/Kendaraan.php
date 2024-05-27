16f926e8ee0ec452edb5f2d528a3df82215a7852
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-05-28 02:24:25 +0800

    Progress: update add with trashed kendaraan model

--- app/Models/Kendaraan.php
+++ app/Models/Kendaraan.php
@@ -12,27 +12,27 @@ class Kendaraan extends Model
 
     public function jenis_kendaraan()
     {
-        return $this->belongsTo(JenisKendaraan::class, 'jenis_kendaraans_id');
+        return $this->belongsTo(JenisKendaraan::class, 'jenis_kendaraans_id')->withTrashed();
     }
 
     public function brand_kendaraan()
     {
-        return $this->belongsTo(BrandKendaraan::class, 'brand_kendaraans_id');
+        return $this->belongsTo(BrandKendaraan::class, 'brand_kendaraans_id')->withTrashed();
     }
 
     public function seri_kendaraan()
     {
-        return $this->belongsTo(SeriKendaraan::class, 'seri_kendaraans_id');
+        return $this->belongsTo(SeriKendaraan::class, 'seri_kendaraans_id')->withTrashed();
     }
 
     public function kilometer_kendaraan()
     {
-        return $this->belongsTo(KategoriKilometerKendaraan::class, 'kategori_kilometer_kendaraans_id');
+        return $this->belongsTo(KategoriKilometerKendaraan::class, 'kategori_kilometer_kendaraans_id')->withTrashed();
     }
 
     public function pelepasan_pemesanan()
     {
-        return $this->belongsTo(PelepasanPemesanan::class, 'id');
+        return $this->belongsTo(PelepasanPemesanan::class, 'id')->withTrashed();
     }
 
     public function servis(int $kendaraans_id)
