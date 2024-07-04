f2e2bfbec9fa171f491827df29a578198fe96054
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-07-05 07:06:43 +0800

    Progress: Update detail pemesanan page

--- resources/views/pemesanan/detail.blade.php
+++ resources/views/pemesanan/detail.blade.php
@@ -191,4 +191,31 @@
             </div>
         </div>
     </div>
+
+    <script>
+        let tarifSewaHari = document.getElementById('tarif_sewa_hari')
+        tarifSewaHari.value = formatRupiah(tarifSewaHari.value, 'Rp. ');
+
+        let tarifSewaMinggu = document.getElementById('tarif_sewa_minggu')
+        tarifSewaMinggu.value = formatRupiah(tarifSewaMinggu.value, 'Rp. ');
+
+        let tarifSewaBulan = document.getElementById('tarif_sewa_bulan')
+        tarifSewaBulan.value = formatRupiah(tarifSewaBulan.value, 'Rp. ');
+
+        function formatRupiah(angka, prefix) {
+            let number_string = angka.replace(/[^,\d]/g, '').toString(),
+                split = number_string.split(','),
+                sisa = split[0].length % 3,
+                rupiah = split[0].substr(0, sisa),
+                ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+            if (ribuan) {
+                separator = sisa ? '.' : '';
+                rupiah += separator + ribuan.join('.');
+            }
+
+            rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+            return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+        }
+    </script>
 @endsection
