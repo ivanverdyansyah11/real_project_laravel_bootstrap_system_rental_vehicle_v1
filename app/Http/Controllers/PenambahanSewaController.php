56688b745f63f7d00ef7bdfdaa390e9b8ba73778
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-07-05 07:05:37 +0800

    Progress: Update penambahan sewa controller

--- app/Http/Controllers/PenambahanSewaController.php
+++ app/Http/Controllers/PenambahanSewaController.php
@@ -28,34 +28,37 @@ class PenambahanSewaController extends Controller
                 'total_biaya' => 'required|string',
                 'keterangan' => 'nullable|string'
             ]);
-    
+
+            $validatedData['total_biaya'] = str_replace('Rp. ', '', $validatedData['total_biaya']);
+            $validatedData['total_biaya'] = (int) str_replace('.', '', $validatedData['total_biaya']);
+
             $pelepasanPemesanan = PelepasanPemesanan::where('id', $id)->with('kendaraan', 'pemesanan')->latest()->first();
             $pembayaran = PembayaranPemesanan::where('pelepasan_pemesanans_id', $id)->latest()->first();
-    
+
             $validatedData['pelepasan_pemesanans_id'] = $pelepasanPemesanan->id;
             $validatedData['kendaraans_id'] = $pelepasanPemesanan->kendaraan->id;
-    
+
             $tanggal_akhir = Carbon::parse($pelepasanPemesanan->pemesanan->tanggal_akhir);
             $jumlah_hari = (int)$validatedData['jumlah_hari'];
             $tanggal_akhir->addDays($jumlah_hari);
-    
+
             PelepasanPemesanan::where('id', $id)->with('kendaraan', 'pemesanan')->latest()->first()->pemesanan->update([
                 'tanggal_akhir' => $pelepasanPemesanan->pemesanan->tanggal_akhir,
                 'tanggal_akhir_awal' => $tanggal_akhir,
             ]);
-    
+
             $waktu_sewa = $pembayaran->waktu_sewa + (int)$validatedData['jumlah_hari'];
             $total_tarif_sewa = $pembayaran->total_tarif_sewa + (int)$validatedData['total_biaya'];
-    
+
             PembayaranPemesanan::where('pelepasan_pemesanans_id', $id)->latest()->first()->update([
                 'waktu_sewa' => $waktu_sewa,
                 'total_tarif_sewa' => $total_tarif_sewa,
                 'jenis_pembayaran' => 'dp',
             ]);
-    
+
             PenambahanSewa::create($validatedData);
             $penambahanID = PenambahanSewa::latest()->first();
-    
+
             Laporan::create([
                 'penggunas_id' => auth()->user()->id,
                 'relations_id' => $penambahanID->id,
